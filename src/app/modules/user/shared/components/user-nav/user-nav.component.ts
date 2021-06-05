5eb1d4cca59b4e7ef9343922c374180ace9dd340
Elvis Edmar Zapatero Astupiñan <elvissoftnife28@hotmail.com>
2021-06-05 00:46:11 -0500

    Agregando ejemplo de llamadas a la api

--- src/app/modules/user/shared/components/user-nav/user-nav.component.ts
+++ src/app/modules/user/shared/components/user-nav/user-nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { UserNavService } from './user-nav.service';
 
 @Component({
   selector: 'app-user-nav',
@@ -10,9 +11,16 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 export class UserNavComponent implements OnInit {
   url = '';
 
-  constructor(private modalService: NgbModal, private router: Router) {}
+  constructor(
+    private modalService: NgbModal,
+    private router: Router,
+    private servicio: UserNavService
+  ) {}
 
-  ngOnInit(): void {}
+  async ngOnInit(): Promise<void> {
+    const data = await this.servicio.getData();
+    console.log(data);
+  }
 
   public redirect(url: string): void {
     this.url = url;
