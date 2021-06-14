4d6310c26a4c2cb3827c904d70bcd75a35d283ab
Juan <juan.tomairo@unmsm.edu.pe>
2021-06-13 20:13:28 -0500

    feat: Listar Programas por Agencia

--- src/app/modules/agency/pages/programs/programs.component.ts
+++ src/app/modules/agency/pages/programs/programs.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { ProgramsService } from './programs.service';
+
+interface Programa {
+  nombre_programa: string;
+  vacantes: number;
+  inscritos: number;
+}
+
+const PROGRAMAS: Programa[] = [
+  {
+    nombre_programa: 'Work and Travel 1',
+    vacantes: 150,
+    inscritos: 20
+  },
+  {
+    nombre_programa: 'Work and Travel 2',
+    vacantes: 150,
+    inscritos: 20
+  },
+  {
+    nombre_programa: 'Work and Travel 3',
+    vacantes: 150,
+    inscritos: 20
+  },
+  {
+    nombre_programa: 'Work and Travel 4',
+    vacantes: 150,
+    inscritos: 20
+  }
+];
+
 
 @Component({
   selector: 'app-programs',
   templateUrl: './programs.component.html',
   styleUrls: ['./programs.component.css']
 })
+
+
+
+
 export class ProgramsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private programa: ProgramsService
+  ) { }
 
-  ngOnInit(): void {
+  
+
+  async ngOnInit(): Promise<void> {
+    const data = await this.programa.getData(); 
+    console.log(data);
   }
 
+  programas = PROGRAMAS;
 }
