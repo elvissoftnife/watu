e82f4ee6980b7b75d70f1f4289ae22cc9cd66ec4
Juan <juan.tomairo@unmsm.edu.pe>
2021-06-13 22:18:20 -0500

    feat: Modal Nuevo Programa

--- src/app/modules/agency/pages/programs/components/add-program-modal/add-program-modal.component.ts
+++ src/app/modules/agency/pages/programs/components/add-program-modal/add-program-modal.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-add-program-modal',
   templateUrl: './add-program-modal.component.html',
   styleUrls: ['./add-program-modal.component.css']
 })
-export class AddProgramModalComponent implements OnInit {
+export class AddProgramModalComponent {
 
-  constructor() { }
+  closeResult = '';
 
-  ngOnInit(): void {
+
+  constructor(private modalService: NgbModal) {}
+
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 }
