bbac2987dad72463637220ddf1b95915e895eeb3
Anthony Raúl Muñante Chávez <anthony.munante.chavez@gmail.com>
2021-06-19 16:58:09 -0500

    porgramas inscripto

--- src/app/modules/user/pages/myprograms/myprograms.component.ts
+++ src/app/modules/user/pages/myprograms/myprograms.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-myprograms',
   templateUrl: './myprograms.component.html',
   styleUrls: ['./myprograms.component.css']
 })
 export class MyprogramsComponent implements OnInit {
+  programs: any[]=[];
+  constructor() { 
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
-
+  ngOnInit(): void {
+    async function getPrograms(){
+      try{
+        const response = await  fetch('https://api-watu.herokuapp.com/vacantes/51');
+        return response.json();
+      }catch(err){
+        console.trace(err);
+      }
+    }
+    getPrograms().then(data=>{
+      
+      this.programs=data.reservas});
+  }
 }
