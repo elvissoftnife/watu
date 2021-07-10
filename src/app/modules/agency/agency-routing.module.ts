7acc40dcfd020b6a6861b52180e310d760460083
elvissoftnife <elvissoftnife28@hotmail.com>
2021-07-10 10:42:18 -0500

    subiendo correcion en las rutas

--- src/app/modules/agency/agency-routing.module.ts
+++ src/app/modules/agency/agency-routing.module.ts
@@ -31,7 +31,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      scrollPositionRestoration: 'enabled'
+    })],
   exports: [RouterModule],
 })
 export class AgencyRoutingModule { }
