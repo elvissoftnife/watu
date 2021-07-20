1ee01f7bbdba56a8a7a7c70cc0f1265a3bf443e7
jhordan <vegachavezjhordan@gmail.com>
2021-07-19 23:55:37 -0500

    feat: activar o desactivar programa de agencia

--- src/app/modules/agency/pages/programs/programs.component.ts
+++ src/app/modules/agency/pages/programs/programs.component.ts
@@ -28,8 +28,7 @@ export class ProgramsComponent implements OnInit {
 
   async obtenerDataPrograma() {
     await this.programService.LoadPrograms();
-    //this.programas = this.programService.programas;
-    console.log(this.programas)
+    console.log(this.programas);
   }
   deleteProgram(event:any, id:number){
     Swal.fire({
@@ -54,6 +53,39 @@ export class ProgramsComponent implements OnInit {
           ).then(() => { this.programService.LoadPrograms(); })
       }
     })
+  }
+
+  changeVisible(event:any, program:any){
+    event.target.checked = program.estado===1?true:false;
+
+    let activar = (program.estado===1?"desactivar":"activar");
+    let activado = (program.estado===1?"desactivado":"activado");
+    
+    Swal.fire({
+      title: "Seguro que desea " +activar+  " el programa?",
+      text: "Se "+(program.id===0?"desactivará":"activará")+" el programa de la agencia.",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: "Sí, "+activar,
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        fetch(`https://api-watu.herokuapp.com/programa/${program.id}`,{
+          method: 'PUT',
+          mode:'cors',
+          cache: 'no-cache'}).then().catch(err=>console.log(err));
+        Swal.fire(
+          activado.toUpperCase()+"!",
+          "El programa ha sido "+activado,
+          'success'
+          ).then(() => { 
+            this.programService.LoadPrograms(); 
+          });
+      }
+      event.target.checked = program.estado===1?true:false;
+    });
   }
 
 }
